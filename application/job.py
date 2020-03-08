fb6739436a9fc2245ab8c7fd861ac26d80d0daaa
arashrai <arashrai17@gmail.com>
2020-03-08 18:59:05 -0400

    minor fix

--- application/job.py
+++ application/job.py
@@ -1,6 +1,6 @@
 class Job:
     def __init__(self, table_name: str, db_name : str, db_host : str, date_col: str):
         self.table_name = table_name
-        self.db_name = name
+        self.db_name = db_name
         self.db_host = db_host
         self.date_col = date_col # String name of the column in the table for which dates are stored
