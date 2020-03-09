efed1ccf771632e1d06064f039e8982b6913203c
arashrai <arashrai17@gmail.com>
2020-03-09 00:54:54 -0400

    maybe this will work

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ chgrp -R mysql /var/lib/mysql
 sudo service mysql start
 sudo service cron start
 mysql -e "CREATE DATABASE daudit_internal"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY ‘rootroot’;"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'rootroot';"
 mysql -e "FLUSH PRIVILEGES;"
 mysql -u root -p${DAUDIT_INTERNAL_PASS} daudit_internal < schema.sql
 exec "$@"
