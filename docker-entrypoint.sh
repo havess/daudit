4f38ef5dc85043e3fbc8198a0a88314d3d35c61c
arashrai <arashrai17@gmail.com>
2020-03-09 00:52:42 -0400

    fix mysql privileges

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,8 @@ chown -R mysql /var/lib/mysql
 chgrp -R mysql /var/lib/mysql
 sudo service mysql start
 sudo service cron start
-mysql -e "create database daudit_internal"
+mysql -e "CREATE DATABASE daudit_internal"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY ‘rootroot’;"
+mysql -e "FLUSH PRIVILEGES;"
 mysql -u root -p${DAUDIT_INTERNAL_PASS} daudit_internal < schema.sql
 exec "$@"
