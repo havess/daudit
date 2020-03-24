8e5a889f49eee1cf83d39bdd6874566cf12a6288
ZainZ <zain-63@hotmail.com>
2020-03-24 01:02:08 -0400

    Print key to test.

--- application/app.py
+++ application/app.py
@@ -220,6 +220,7 @@ def parse_jobs():
 
     # Add to auditQueue
     for work in job_str:
+        print(key)
         key, date_created, channel = work.split(":")
         db_host, db_name, table_name = key.split("/")
         audit_job = Job(
